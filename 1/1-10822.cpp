5f76eaf55f93b0fad2c26cb5e4017d843ef9f5db
Hangju0610 <131460159+Hangju0610@users.noreply.github.com>
2026-08-12 14:47:25 +0900

    add: 4-f

--- 1/1-10822.cpp
+++ 1/1-10822.cpp
@@ -2,22 +2,7 @@
 using namespace std;
 
 string s;
-vector<int> numbers;
 string delimeter = ",";
-
-vector<int> split_num(const string& input, string delimiter) {
-    vector<int> result;
-    auto start = 0; // int 로 추론
-    auto end = input.find(delimiter); // size_t로 추론
-    while (end != string::npos) {
-        int temp = atoi(input.substr(start, end = start).c_str());
-        result.push_back(temp);
-        start = end + delimiter.size();
-        end = input.find(delimiter, start);
-    }
-    result.push_back(atoi(input.substr(start).c_str()));
-    return result;
-}
 
 int main() {
     ios::sync_with_stdio(false);
@@ -29,7 +14,7 @@ int main() {
     auto end = s.find(delimeter);
     while(end != string::npos) {
         string temp_str = s.substr(start, end - start);
-        cout << temp_str << "\n";
+        // cout << temp_str << "\n";
         int temp = atoi(temp_str.c_str());
         result += temp;
         start = end + delimeter.size();
